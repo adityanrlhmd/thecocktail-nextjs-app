c4f7af0f82a9628a57afe069a95d12a0adab43e6
adityanrlhmd <adityanurulhamda@gmail.com>
2022-05-20 20:28:44 +0700

    make speedDial for nav

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'main-pattern' : "url('https://images.unsplash.com/photo-1514361892635-6b07e31e75f9?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870')"
+      }
+    },
+  },
+  plugins: [],
+}
